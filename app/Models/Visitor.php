cb5712607d98c05a2435c60398c7c1fb24de684d
Alif Khaerudin <alifkha7@live.com>
2022-09-24 14:03:15 +0700

    first commit

---
+++ app/Models/Visitor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Visitor extends Model
+{
+    use HasFactory;
+
+    public $timestamps = FALSE;
+
+    protected $fillable = [
+        'date',
+        'ip'
+    ];
+
+    protected $casts = [
+        'date' => 'datetime:Y-m-d',
+    ];
+}
